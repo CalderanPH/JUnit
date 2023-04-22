7dd7185e7ac3af19cba1821720c83985b2bd16e5
Paulo Henrique M. Calderan <paulo_calderam@hotmail.com>
2023-04-22 15:55:46 -0300

    Refactor method name

--- src/main/java/Funcionario.java
+++ src/main/java/Funcionario.java
@@ -1,20 +1,68 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Funcionario {
-    private String nome;
-    private String email;
-    private double salarioBase;
-    private String cargo;
-    private List<Funcionario> funcionarioList;
+
+    private final String nome;
+    private final String email;
+    private final double salarioBase;
+    private final String cargo;
+    private final List<Funcionario> funcionarioList;
 
     public Funcionario(String nome, String email, double salarioBase, String cargo) {
         this.nome = nome;
         this.email = email;
         this.salarioBase = salarioBase;
         this.cargo = cargo;
-        this.funcionarioList = new ArrayList<Funcionario>();
+        this.funcionarioList = new ArrayList<>();
+    }
+
+    public double salarioDesenvolvedor() {
+        if (salarioBase >= 3.000) {
+            double devPor = 20.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        } else {
+            double devPor = 10.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        }
+    }
+
+    public double salarioDBA() {
+        if (salarioBase >= 2.000) {
+            double devPor = 25.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        } else {
+            double devPor = 15.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        }
+    }
+
+    public double salarioQA() {
+        if (salarioBase >= 2.000) {
+            double devPor = 25.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        } else {
+            double devPor = 15.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        }
+    }
+
+    public double salarioGerente() {
+        if (salarioBase >= 5.000) {
+            double devPor = 30.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        } else {
+            double devPor = 20.0 / 100.0;
+            return salarioBase - (devPor * salarioBase);
+        }
+    }
+
+    public void salvaFuncionario(Funcionario funcionario) {
+        funcionarioList.add(funcionario);
+    }
+
+    public List<Funcionario> getFuncionarioList() {
+        return funcionarioList;
     }
 
     public String getNome() {
@@ -31,62 +79,6 @@ public class Funcionario {
 
     public String getCargo() {
         return cargo;
-    }
-
-    public double calcDesenvolvedor() {
-        if (salarioBase >= 3.000) {
-            double devPor = 20.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        } else {
-            double devPor = 10.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        }
-    }
-
-    public double calcDBA() {
-        if (salarioBase >= 2.000) {
-            double devPor = 25.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        } else {
-            double devPor = 15.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        }
-    }
-
-    public double calcTest() {
-        if (salarioBase >= 2.000) {
-            double devPor = 25.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        } else {
-            double devPor = 15.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        }
-    }
-
-    public double calcGeren() {
-        if (salarioBase >= 5.000) {
-            double devPor = 30.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        } else {
-            double devPor = 20.0 / 100.0;
-            double salarioDev = salarioBase - (devPor * salarioBase);
-            return salarioDev;
-        }
-    }
-
-    public void salvaFuncionario(Funcionario funcionario) {
-        funcionarioList.add(funcionario);
-    }
-
-    public List<Funcionario> getFuncionarioList() {
-        return Collections.unmodifiableList(funcionarioList);
     }
 
 }
--- src/test/java/FuncionarioTest.java
+++ src/test/java/FuncionarioTest.java
@@ -4,13 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FuncionarioTest {
 
-
     @Test
     public void descontaSalarioDevMais() {
         Funcionario joao = new Funcionario(
                 "Joao", "joao@joao.com", 5.000, "Desenvolvedor");
         joao.salvaFuncionario(joao);
-        double salario = joao.calcDesenvolvedor();
+        double salario = joao.salarioDesenvolvedor();
         assertEquals(1, joao.getFuncionarioList().size());
         assertEquals(4.000, salario, 0.00001);
     }
@@ -20,18 +19,17 @@ public class FuncionarioTest {
         Funcionario joao = new Funcionario(
                 "Joao", "joao@joao.com", 2.000, "Desenvolvedor");
         joao.salvaFuncionario(joao);
-        double salario = joao.calcDesenvolvedor();
+        double salario = joao.salarioDesenvolvedor();
         assertEquals(1, joao.getFuncionarioList().size());
         assertEquals(1.800, salario, 0.00001);
     }
-
 
     @Test
     public void descontaSalarioDBAMais() {
         Funcionario pedro = new Funcionario(
                 "Pedro", "pedro@pedro.com", 3.500, "DBA");
         pedro.salvaFuncionario(pedro);
-        double salario = pedro.calcDBA();
+        double salario = pedro.salarioDBA();
         assertEquals(1, pedro.getFuncionarioList().size());
         assertEquals(2.625, salario, 0.00001);
     }
@@ -41,7 +39,7 @@ public class FuncionarioTest {
         Funcionario pedro = new Funcionario(
                 "Pedro", "pedro@pedro.com", 1.500, "DBA");
         pedro.salvaFuncionario(pedro);
-        double salario = pedro.calcDBA();
+        double salario = pedro.salarioDBA();
         assertEquals(1, pedro.getFuncionarioList().size());
         assertEquals(1.275, salario, 0.00001);
     }
@@ -51,7 +49,7 @@ public class FuncionarioTest {
         Funcionario maria = new Funcionario(
                 "Maria", "maria@maria.com", 2.000, "Testador");
         maria.salvaFuncionario(maria);
-        double salario = maria.calcTest();
+        double salario = maria.salarioQA();
         assertEquals(1, maria.getFuncionarioList().size());
         assertEquals(1.500, salario, 0.00001);
     }
@@ -61,18 +59,17 @@ public class FuncionarioTest {
         Funcionario maria = new Funcionario(
                 "Maria", "maria@maria.com", 1.500, "Testador");
         maria.salvaFuncionario(maria);
-        double salario = maria.calcTest();
+        double salario = maria.salarioQA();
         assertEquals(1, maria.getFuncionarioList().size());
         assertEquals(1.275, salario, 0.00001);
     }
-
 
     @Test
     public void descontaSalarioGerenMais() {
         Funcionario rosa = new Funcionario(
                 "Rosa", "rosa@rosa.com", 5.000, "Gerente");
         rosa.salvaFuncionario(rosa);
-        double salario = rosa.calcGeren();
+        double salario = rosa.salarioGerente();
         assertEquals(1, rosa.getFuncionarioList().size());
         assertEquals(3.500, salario, 0.00001);
     }
@@ -82,7 +79,7 @@ public class FuncionarioTest {
         Funcionario rosa = new Funcionario(
                 "Rosa", "rosa@rosa.com", 3.000, "Gerente");
         rosa.salvaFuncionario(rosa);
-        double salario = rosa.calcGeren();
+        double salario = rosa.salarioGerente();
         assertEquals(1, rosa.getFuncionarioList().size());
         assertEquals(2.400, salario, 0.00001);
     }
